cfdd02feb6bb12baf0b15eb44efade86de9a79e8
Regina Gimazova <reginagimazova98@gmail.com>
2018-05-18 03:46:55 +0300

    version2

--- Snakes_game/PHPSnake/Snake.php
+++ Snakes_game/PHPSnake/Snake.php
@@ -15,11 +15,12 @@ class Snake
     private $id;
     private $head, $body, $tail;
     private $length = 5;
-    private $steps_count = 50;
 
-    private $is_alive = false;
+    private $is_alive = false; // для каждой змеи, в общем классе проверять, если одна из змей умерла, game over
+
     private $is_bited = false;
     private $is_crashed = false;
+    private $steps_count = 50;
 
     private $score = 0;
     private $json;
@@ -32,17 +33,92 @@ class Snake
         }
         return $this->is_alive;
     }
-    public function _construct($id, $x, $y){
+
+    public function _construct($id, $k){
 
         $this->id = $id;
 
-        $this->head = new Location($x, $y);
+        if ($k == 1){
+            $this->head = $this->generateLocationForFirstSnake();
+        }
+        elseif ($k == 2){
+            $this->head = $this->generateLocationForSecondSnake();
+        }
 
+        $x = $this->head->getX();
+        $y = $this->head->getY();
+
+        //проверка рандома для разного положения змей
         for ($i = 1; $i <= 3; $i++){
             $this->body[$i] = new Location($x + $i, $y);
         }
 
         $this->tail = new Location($x + 4, $y);
+    }
+
+    /**
+     * @return Location
+     */
+    public function getHead()
+    {
+        return $this->head;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getTail()
+    {
+        return $this->tail;
+    }
+
+    /**
+     * @param Location $head
+     */
+    public function setHead($head): void
+    {
+        $this->head = $head;
+    }
+
+    /**
+     * @param Location $body
+     */
+    public function setBody($body): void
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @param Location $tail
+     */
+    public function setTail($tail): void
+    {
+        $this->tail = $tail;
+    }
+
+
+    // расположение рандомное: одна змея (х:0 до 6; у:0 до 4), другая змея (х:4 до 10; у:6 до 10)
+    //проверка рандома на границе
+    public function generateLocationForFirstSnake(){
+        $x = rand(0,6);
+        $y = rand(0,4);
+        $location = new Location($x, $y);
+        return $location;
+    }
+
+    public function generateLocationForSecondSnake(){
+        $x = rand(4,10);
+        $y = rand(6,10);
+        $location = new Location($x, $y);
+        return $location;
     }
 
     public function getId()
@@ -57,8 +133,16 @@ class Snake
         return $json_string;
     }
 
-    public function testBoard(){
-
+    //проверка границ
+    public function testBoard()
+    {
+        $this->head = $this->getHead();
+        $x = $this->head->getX();
+        $y = $this->head->getY();
+        if ($x == 0 || $y == 0 || $x == 10 || $y == 10){
+            $this->is_crashed = true;
+        }
+        return $this->is_crashed;
     }
 
 
@@ -114,10 +198,10 @@ class Snake
                 $this->head = new Location($x, $y + 1);
                 $this->tail = new Location($x, $y + 1);
                 break;
-        // если делать в консоли, надо почистить клетку, где змея была, сделать ее пустой
         }
     }
 
+    // подумать как взять координаты змей
     private function snake_choose_dir(){
         $x_snake = 0;
         $y_snake = 0;
@@ -184,55 +268,10 @@ class Snake
         }
     }
 
+    //если координаты головы одной змеи равны координатам хвоста другой змеи, то откусываем
     private function eatSnake(){
 
     }
 
 }
 
-/*
-    function move($dir){
-        switch ($dir){
-            case 'LEFT':
-                $this.x -= 1;
-            break;
-            ....
-        }
-    }
-}*/
-
-    /*public function move(string $input)
-{
-    $this->changeDirection($input);
-    $row = $this->points[0]->getRow();
-    $col = $this->points[0]->getCol();
-    switch ($this->direction) {
-        case Direction::RIGHT:
-            $col++;
-            break;
-        case Direction::LEFT:
-            $col--;
-            break;
-        case Direction::UP:
-            $row--;
-            break;
-        case Direction::DOWN:
-            $row++;
-            break;
-    }
-    if ($col >= $this->boardCols - 1) {
-        $col = 1;
-    } elseif ($col < 1) {
-        $col = $this->boardCols - 2;
-    }
-    if ($row >= $this->boardRows - 1) {
-        $row = 1;
-    } elseif ($row < 1) {
-        $row = $this->boardRows - 2;
-    }
-    $this->points[0]->setChar(Char::shadeBlock());
-    $next = new Location($row, $col, Char::block());
-    $this->checkCollision($next);
-    array_unshift($this->points, $next);
-    $this->lastPoint = array_pop($this->points);
-}*/
